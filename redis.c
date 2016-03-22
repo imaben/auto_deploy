816900cbf7b9147112dd091d9db1cdca9a12cc3f
马犇 <benma@angejia.com>
2016-03-22 20:05:09 +0800

    fixed bug

--- redis.c
+++ redis.c
@@ -48,20 +48,18 @@ int redis_hash_set(char *key, char *field, char *value)
     smart_str_appendl(&command, key, strlen(key));
     smart_str_appendl(&command, " ", 1);
     smart_str_appendl(&command, field, strlen(field));
-    smart_str_appendl(&command, " ", 1);
+    smart_str_appendl(&command, " \"", 2);
     smart_str_appendl(&command, value, strlen(value));
+    smart_str_appendl(&command, "\"", 1);
     smart_str_0(&command);
     reply = redisCommand(g_redis, command.c);
-    smart_str_free(&command);
-    if (g_redis->err != 0
-            || reply == NULL
-            || reply->type != REDIS_REPLY_INTEGER)
-    {
+    if (g_redis->err != 0) {
         // todo write warining log
-        fprintf(stderr, "redis hash set failure\n");
+        fprintf(stderr, "redis hash set failure, error:%d, command:%s\n", g_redis->err, command.c);
     } else {
         r = (int)reply->integer;
     }
+    smart_str_free(&command);
 
     freeReplyObject(reply);
     return r;
@@ -83,9 +81,7 @@ int redis_append(char *key, char *value)
     smart_str_0(&command);
     reply = redisCommand(g_redis, command.c);
     smart_str_free(&command);
-    if (g_redis->err != 0
-            || reply == NULL
-            || reply->type != REDIS_REPLY_INTEGER)
+    if (g_redis->err != 0)
     {
         // todo write warining log
         fprintf(stderr, "redis append failure\n");
